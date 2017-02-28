494a562ad1792048490918b8c1cb0c31639ae9b8
iam91 <zhengwy91@foxmail.com>
2017-02-28 19:57:19 +0800

    test passed

--- src/zpromise.js
+++ src/zpromise.js
@@ -1,130 +1,152 @@
+'use strict';
 
-  'use strict';
+var PENDING = 0;
+var FULFILLED = 1;
+var REJECTED = 2;
 
-  var PENDING   = 0;
-  var FULFILLED = 1;
-  var REJECTED  = 2;
+var Fallback = {
+    onFulfilled: function(v) {
+        return v;
+    },
+    onRejected: function(r) {
+        throw r;
+    }
+};
 
-  function Zpromise(resolver){
+function Zpromise(resolver) {
     var state = PENDING;
     var value = null;
-    var done = false;
+
+    this.s = state;
 
     var callbacks = [];
+    this.c = callbacks;
 
     var self = this;
-    function resolve(x){
-      try{
-        if(self === x){ reject(new TypeError('Promise and x refer to the same object')); }
-        else if(x instanceof Zpromise){
-          x.then(function(v){
-            fulfill(v);
-          }, function(r){
-            reject(r);
-          });
-        }else if((typeof x === 'object' || typeof x === 'function')
-          && x && typeof x.then === 'function'){
-          //typeof null === 'object'
-          x.then(function(y){
-            if(done) return;
-            done = true;
-            resolve(y);
-          }, function(r){
-            if(done) return;
-            done = true;
-            reject(r);
-          });
-        }else{
-          fulfill(x);
+
+    function resolve(x) {
+        if (self === x) {
+            reject(new TypeError('Promise and x refer to the same object.'));
+        } else if (x instanceof Zpromise) {
+            x.then(function(v) {
+                resolve(v);
+            }, function(r) {
+                reject(r);
+            });
+        } else if (typeof x === 'object' || typeof x === 'function') {
+            var called = false;
+            var then = null;
+            try{
+                if (x && typeof (then = x.then) === 'function') {
+                    then.bind(x)(function resolvePromise(y) {
+                        if (called) return;
+                        called = true;
+                        resolve(y);
+                    }, function rejectPromise(r) {
+                        if (called) return;
+                        called = true;
+                        reject(r);
+                    });
+                } else {
+                    fulfill(x);
+                }
+            }catch(e){
+                if(!called) reject(e);
+            }
+        } else {
+            fulfill(x);
         }
-      }catch(e){
-        if(!done) reject(e);
-      }
     }
 
-    function fulfill(v){
-      if(state === PENDING){
-        state === FULFILLED;
-        value = v;
-        runCallbacks();
-      }
-    }
-
-    function reject(r){
-      if(state === PENDING){
-        state === REJECTED;
-        value = r;
-        runCallbacks();
-      }
-    }
-
-    function runCallbacks(){
-      if(state === PENDING) return;
-      setTimeout(function(){
-        while(callbacks.length){
-          var callback = callbacks.shift();
-          var resolveThat = callback.resolveThat;
-          var rejectThat = callback.rejectThat;
-
-          var handler = state === FULFILLED ? callback.onFulfilled : callback.onRejected;
-          if( typeof handler === 'function'){
-            try{ resolveThat(handler(value)); }
-            catch(e){ rejectThat(e); }
-          }else{ (state === FULFILLED ? resolveThat : rejectThat)(value); }
+    function fulfill(v) {
+        if (state === PENDING) {
+            state = FULFILLED;
+            value = v;
+            done();
+            self.s = state;
         }
-      }, 0);
+    }
+
+    function reject(r) {
+        if (state === PENDING) {
+            state = REJECTED;
+            value = r;
+            done();
+            self.s = state;
+        }
+    }
+
+    function done() {
+        if (state === PENDING) return;
+        setTimeout(function(){
+            while (callbacks.length) {
+                var callback = callbacks.shift();
+                var resolveThat = callback.resolveThat;
+                var rejectThat = callback.rejectThat;
+
+                var handler = state === FULFILLED ? callback.onFulfilled : callback.onRejected;
+                var fallback = state === FULFILLED ? Fallback.onFulfilled : Fallback.onRejected;
+                handler = handler || fallback;
+                try {
+                    resolveThat(handler(value));
+                } catch (e) {
+                    rejectThat(e);
+                }
+            }
+        }, 0);
     }
 
     function promise(onFulfilled, onRejected, resolveThat, rejectThat) {
-      callbacks.push({
-        onFulfilled: onFulfilled,
-        onRejected: onRejected,
-        resolveThat: resolveThat,
-        rejectThat: rejectThat
-      });
-      runCallbacks();
+        callbacks.push({
+            onFulfilled: onFulfilled,
+            onRejected: onRejected,
+            resolveThat: resolveThat,
+            rejectThat: rejectThat
+        });
+        done();
     };
 
     Object.defineProperty(this, 'promise', {
-      enumerable: false,
-      configurable: false,
-      writable: false,
-      value: promise
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: promise
     });
     //execute the resolver
-    resolver(resolve, reject);
-  }
+    try {
+        resolver(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
+}
 
-  Zpromise.prototype.then = function(onFulfilled, onRejected){
+Zpromise.prototype.then = function(onFulfilled, onRejected) {
     var that = this;
-    return new Zpromise(function(resolve, reject){
-      that.promise(onFulfilled, onRejected, resolve, reject);
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : null;
+    onRejected = typeof onRejected === 'function' ? onRejected : null;
+    return new Zpromise(function(resolve, reject) {
+        that.promise(onFulfilled, onRejected, resolve, reject);
     });
-  };
+};
 
-  Zpromise.prototype.catch = function(onRejected){
-    var that = this;
-    return new Zpromise(function(resolve, reject){
-      that.promise(void 0, onRejected, resolve, reject);
+Zpromise.prototype.catch = function(onRejected) {
+    this.then(void 0, onRejected);
+};
+
+Zpromise.all = function(iterable) {};
+
+Zpromise.race = function(iterable) {};
+
+Zpromise.reject = function(reason) {
+    return new Zpromise(function(resolve, reject) {
+        reject(reason);
     });
-  };
+};
 
-  Zpromise.all = function(iterable){
-  };
-
-  Zpromise.race = function(iterable){
-  };
-
-  Zpromise.reject = function(reason){
-    return new Zpromise(function(resolve, reject){
-      reject(reason);
+Zpromise.resolve = function(value) {
+    return new Zpromise(function(resolve, reject) {
+        resolve(value);
     });
-  };
+};
 
-  Zpromise.resolve = function(value){
-    return new Zpromise(function(resolve, reject){
-      resolve(value);
-    });
-  };
-
-  module.exports = Zpromise;
+module.exports = Zpromise;
--- test/adapter.js
+++ test/adapter.js
@@ -1,24 +1,15 @@
 var Zpromise = require('../src/zpromise');
 
 module.exports = {
-  resolved: function(value){
-    return Zpromise.resolve(value);
-  },
-
-  rejected: function(reason){
-    return Zpromise.reject(reason);
-  },
-
-  deferred: function(){
-    var resolve, reject;
-
-    return {
-      promise: new Zpromise(function(rslv, rjct){
-        resolve = rslv;
-        reject = rjct;
-      }),
-      resolve: resolve,
-      reject: reject
-    };
-  }
+    deferred: function() {
+        var resolve, reject;
+        return {
+            promise: new Zpromise(function(rslv, rjct){
+                resolve = rslv;
+                reject = rjct;
+            }),
+            resolve: resolve,
+            reject: reject
+        };
+    }
 };
